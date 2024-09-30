ac765ba5be31d26f43a8d8086982971429d70351
Abderrahim <b00155703@mytudublin.ie>
2024-09-30 12:56:20 +0100

    adding code to the file

---
+++ Abderrahim/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+// Base class
+#define pi      3.14
+class Shape
+{
+    public:
+        // pure virtual function providing interface framework.
+        virtual int getArea() = 0;
+        void setWidth(int w){
+            width = w;
+        }
+        void setHeight(int h){
+            height = h;
+        }
+    protected:
+        int width;
+        int height;
+};
+class Triangle: public Shape
+{
+    public:
+       int getArea(){
+        return (width * height)/2;
+        }
+};
+class Circle: public Shape{
+    public:
+    int getArea(){
+        return(pi*width);
+    }
+};
+
+int main(void)
+{
+    Triangle Tri;
+    Circle Cir;
+    Cir.setWidth(5);
+    Tri.setWidth(5);
+    Tri.setHeight(7);
+    // Print the area of the object.
+    cout << "Total Triangle area: " << Tri.getArea() << endl;
+    cout << "Total Circle area: " << Cir.getArea() << endl;
+    return 0;
+}
